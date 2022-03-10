d6fc274116b0da6f850732b01fdeaed1e154e031
rodrigo.paulino <rodrigo.paulino@prima.com.br>
2022-03-10 17:49:25 -0300

    Incluir migration

--- src/services/NSE.Carrinho.API/Data/CarrinhoContext.cs
+++ src/services/NSE.Carrinho.API/Data/CarrinhoContext.cs
@@ -1,15 +1,41 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NSE.Carrinho.API.Models;
 using NSE.Core.Data;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace NSE.Carrinho.API.Data
 {
-	public class CarrinhoContext : DbContext, IUnitOfWork
+	public class CarrinhoContext : DbContext
 	{
 		public CarrinhoContext(DbContextOptions<CarrinhoContext> options)
 			: base(options)
-		{ }
+		{
+			ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+			ChangeTracker.AutoDetectChangesEnabled = false;
+		}
 
-		public async Task<bool> Commit() => await SaveChangesAsync() > 0;
+		public DbSet<CarrinhoItem> CarrinhoItens { get; set; }
+		public DbSet<CarrinhoCliente> CarrinhoCliente { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+				e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+				property.SetColumnType("varchar(100)");
+
+			modelBuilder.Entity<CarrinhoCliente>()
+				.HasIndex(c => c.ClienteId)
+				.HasName("IDX_Cliente");
+
+			modelBuilder.Entity<CarrinhoCliente>()
+				.HasMany(c => c.Itens)
+				.WithOne(c => c.CarrinhoCliente)
+				.HasForeignKey(c => c.CarrinhoId);
+
+			foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(
+				e => e.GetForeignKeys()))
+				relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+		}
 	}
 }
