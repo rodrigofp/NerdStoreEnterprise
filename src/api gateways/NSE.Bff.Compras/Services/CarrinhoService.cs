71328628b4459340ff7fed18d8e2f279ea94cff9
Rodrigo Paulino <rodrigo_fp@ymail.com>
2022-06-02 19:49:11 -0300

    Inclusão API Gateway

---
+++ src/api gateways/NSE.Bff.Compras/Services/CarrinhoService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+using NSE.Bff.Compras.Extensions;
+using System;
+using System.Net.Http;
+
+namespace NSE.Bff.Compras.Services
+{
+	public interface ICarrinhoService { }
+
+	public class CarrinhoService : Service, ICarrinhoService
+	{
+		private readonly HttpClient _httpClient;
+
+		public CarrinhoService(HttpClient httpClient, IOptions<AppServiceSettings> settings)
+		{
+			_httpClient = httpClient;
+			_httpClient.BaseAddress = new Uri(settings.Value.CarrinhoUrl);
+		}
+	}
+}
